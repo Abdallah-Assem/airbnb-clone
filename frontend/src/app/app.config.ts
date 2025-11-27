16ae0c3b608b78c6676666247363f8a55b79b249
aaliyahhazem <aaliyahhazem@gmail.com>
2025-11-27 22:53:29 +0200

    Editing ListingOverviewVm - update listing service - binding frontend with backend for listings page and host page

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -10,7 +10,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
   ]
 };
